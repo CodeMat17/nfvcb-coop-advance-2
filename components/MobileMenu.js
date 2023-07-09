ea8d1e61ebc946b2198a4de6b8eb3886d63da67f
CodeMat <codemat.biz@gmail.com>
2023-07-09 14:09:57 +0100

    updated

--- components/MobileMenu.js
+++ components/MobileMenu.js
@@ -75,17 +75,17 @@ const MobileMenu = async ({ session, profile }) => {
 
                   {profile &&
                     profile.map((user) => (
-                      <div key={user.id}>
+                      <Menu.Item key={user.id}>
                         {user.isAdmin && (
-                          <Menu.Item>
+                      
                             <Link
-                              href='/admin'
-                              className='p-4 ui-active:bg-blue-500 ui-active:text-white ui-not-active:bg-gray-300 dark:ui-not-active:bg-gray-600 ui-not-active:text-black dark:ui-not-active:text-white shadow-lg'>
+                              className='p-4 ui-active:bg-blue-500 ui-active:text-white ui-not-active:bg-gray-300 dark:ui-not-active:bg-gray-600 ui-not-active:text-black dark:ui-not-active:text-white shadow-lg'
+                              href='/admin'>
                               ADMIN
                             </Link>
-                          </Menu.Item>
+                       
                         )}
-                      </div>
+                      </Menu.Item>
                     ))}
 
                   <Menu.Item>
